e466f790c96c12223b2b0272bbc95441b0fe914c
karu <karu@pop-os.localdomain>
2026-04-07 18:31:24 +0200

    viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,24 @@
 import { defineConfig } from 'vite'
 import mkcert from 'vite-plugin-mkcert'
 
-export default defineConfig({
-  plugins: [
-    mkcert({
-      hosts: ['192.168.1.133', 'localhost'] 
-    })
-  ],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    https: true,
-		proxy: {
-      '/api': 'http://127.0.0.1:3000',
-      '/register-token': 'http://127.0.0.1:3000',
-      '/test-push': 'http://127.0.0.1:3000',
+export default defineConfig(({ command }) => {
+  return {
+    base: command === 'build' ? '/Abuelos/' : '/',
+
+    plugins: [
+      // Solo activamos mkcert en desarrollo
+      command === 'serve' ? mkcert({ hosts: ['192.168.1.133', 'localhost'] }) : []
+    ],
+
+    server: {
+      host: '0.0.0.0',
+      port: 5173,
+      https: true,
+      proxy: {
+        '/api': 'http://127.0.0.1:3000',
+        '/register-token': 'http://127.0.0.1:3000',
+        '/test-push': 'http://127.0.0.1:3000',
+      }
     }
   }
 })
